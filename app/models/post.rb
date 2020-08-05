960cf3f3ac45ac63da8816257a4a2ff12f1b6471
oloomoses <olooine@gmail.com>
2020-08-06 00:05:33 +0300

    create association and post validations with their test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 10, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 30, maximum: 300 }
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_160141) do
+ActiveRecord::Schema.define(version: 2020_08_05_203013) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_160141) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
